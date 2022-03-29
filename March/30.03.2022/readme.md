bc550a889613bf259b0426d34cfad54199d07468
Ghassanooooo <ghassanaldarwish@gmail.com>
2022-03-29 09:11:44 +0200

    30.03.2022

---
+++ March/30.03.2022/readme.md
@@ -0,0 +1,12 @@
+# Wednesday 30.03.2022
+  
+  **Classes**
+   - Classes Revision
+---
+
+**Assignment:** [ PB-refactor-to-class](https://classroom.github.com/a/iDA0EPE4)
+
+
+  
+
+ 
---
+++ March/30.03.2022/refactor-todos-app.js
@@ -0,0 +1,41 @@
+class ToDos {
+    //###### SET PROPERTIES######
+   //1- Set toDosStore prop array in the constructor method 
+   // toDosStore = ["Shopping", "Meet with PR department", "Pay bills"];
+   
+     
+     
+   //###### READ ######
+   /**
+    2- Create `renderToDosListTemplate` method to read todos and render in the following format:
+ 
+     ToDo List: 
+     1- Shopping 
+     2- Meet with PR department 
+     3- Pay bills 
+    */
+    renderToDosListTemplate(){}
+ 
+ 
+   //##### CREATE ######
+   //3- Create a method named `addOne` to add new item to toDosStore.
+    addOne() { }
+    
+   //4- Create a method named `addMany` to add many new items to toDosStore.
+    addMany() { } 
+    
+   //##### DELETE ######
+   //5- Create a method named `deleteOne` to remove any item by order number. Passing the item order number as argument.
+   deleteOne(){}
+ 
+     
+   //##### EDIT ######
+   //6- Create a method named `editOne` to edit any item by order number. Passing the item order number as a first argument and the new value as a second argument.
+   editOne(){}
+ }
+ 
+ 
+ 
+ 
+ // Call renderToDosListTemplate method in the end of the current file.
+ 
